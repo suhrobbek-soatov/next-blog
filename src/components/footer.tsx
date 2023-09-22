b7caeca83ce093957e02d454186f1b0f51cede6e
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-22 22:31:17 +0500

    Add completely footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,30 @@
+import { Instagram, Telegram, YouTube } from "@mui/icons-material";
+import { Box, Button, ButtonGroup, Typography } from "@mui/material";
+import { format } from "date-fns";
 import { FC } from "react";
 
 const Footer: FC = (): JSX.Element => {
-  return <div>Footer</div>;
+  return (
+    <Box
+      component="footer"
+      color="white"
+      bgcolor="primary.main"
+      sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", p: "20px" }}
+    >
+      <Typography>©️ {format(new Date(), "yyyy")} BLOG. All Right Reserved!</Typography>
+      <ButtonGroup disableElevation variant="contained" aria-label="socials">
+        <Button href="#">
+          <Telegram />
+        </Button>
+        <Button href="#">
+          <Instagram />
+        </Button>
+        <Button href="#">
+          <YouTube />
+        </Button>
+      </ButtonGroup>
+    </Box>
+  );
 };
 
 export default Footer;
