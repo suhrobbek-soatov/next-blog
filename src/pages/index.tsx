92deab502d9996e300a08a63ef6a208069fe7147
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-23 17:49:12 +0500

    Make responsive index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,10 @@ const Home = () => {
       </Head>
       <Hero />
       <Grid container spacing="20px" mb="20px" paddingX="15px">
-        <Grid item xs={3.5}>
+        <Grid item xs={12} lg={3.5}>
           <Sidebar />
         </Grid>
-        <Grid item xs={8.5}>
+        <Grid item xs={12} lg={8.5}>
           <Content />
         </Grid>
       </Grid>
