8dcf1553e7c8f6ebb2d9b8161f20cbeb335c2616
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-04 16:52:36 +0500

    Delete unnecessary data variable

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -8,28 +8,7 @@ export const navLinks = [
     text: "Category",
   },
   {
-    route: "/blogs",
+    route: "/blog",
     text: "Blogs",
-  },
-];
-
-export const data = [
-  {
-    image: "https://media.graphassets.com/MxJZhmooRRuudoErkQ38",
-    title: "Technical SEO with Hygraph",
-    exerpt: "Get started with your SEO implementation when using a Headless CMS",
-    author: {
-      name: "Suhrobbek Soatov",
-      avatar: "https://media.graphassets.com/DkfNqQNGRz2F4UFntKQx",
-    },
-  },
-  {
-    image: "https://media.graphassets.com/bh3K2NNtTHCN260Xfq9h",
-    title: "Union Types and Sortable Relations with Hygraph",
-    exerpt: "Learn more about Polymorphic Relations and Sortable Relations with Hygraph",
-    author: {
-      name: "Suhrobbek Soatov",
-      avatar: "https://media.graphassets.com/DkfNqQNGRz2F4UFntKQx",
-    },
   },
 ];
