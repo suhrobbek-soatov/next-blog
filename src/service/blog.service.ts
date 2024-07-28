1dafdfe96014f0d8af121dd7a492f966cf68e235
svkhrobbeck <svkhrobbeck@gmail.com>
2024-07-28 05:09:06 +0500

    fix: type error

--- src/service/blog.service.ts
+++ src/service/blog.service.ts
@@ -1,7 +1,7 @@
 import { IBlog, IBlogCategory } from "@/interfaces/blogs.interface";
 import { request, gql } from "graphql-request";
 
-const graphqlAPI = process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT;
+const graphqlAPI = process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT as string;
 
 export const BlogsService = {
   async getAllBlogs() {
